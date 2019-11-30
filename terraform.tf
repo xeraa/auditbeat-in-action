3a82c50ddd0933f06036aeb929a10ea648ce011d
Philipp Krenn <pk@xeraa.net>
2019-11-30 14:17:26 +0100

    CHANGE Terraform to the new syntax

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 provider "aws" {
     # Credentials are defined in the environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-    region = "${var.region}"
+    region = var.region
 }
 
 
 # Create the SSH key pair
 resource "aws_lightsail_key_pair" "auditd_key_pair" {
   name       = "auditd_key_pair"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 
@@ -15,48 +15,48 @@ resource "aws_lightsail_key_pair" "auditd_key_pair" {
 resource "aws_lightsail_instance" "instance" {
   name              = "instance"
   availability_zone = "${var.region}a"
-  blueprint_id      = "${var.operating_system}"
-  bundle_id         = "${var.size}"
+  blueprint_id      = var.operating_system
+  bundle_id         = var.size
   key_pair_name     = "auditd_key_pair"
-  depends_on        = ["aws_lightsail_key_pair.auditd_key_pair"]
+  depends_on        = [aws_lightsail_key_pair.auditd_key_pair]
 }
 resource "aws_route53_record" "apex" {
-  zone_id = "${var.zone_id}"
-  name    = "${var.domain}"
+  zone_id = var.zone_id
+  name    = var.domain
   type    = "A"
   ttl     = "60"
-  records = ["${aws_lightsail_instance.instance.public_ip_address}"]
+  records = [aws_lightsail_instance.instance.public_ip_address]
 }
 resource "aws_route53_record" "www" {
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
   name    = "www.${var.domain}"
   type    = "A"
   alias {
-    name                   = "${var.domain}"
-    zone_id                = "${var.zone_id}"
+    name                   = var.domain
+    zone_id                = var.zone_id
     evaluate_target_health = false
   }
-  depends_on = ["aws_route53_record.apex"]
+  depends_on = [aws_route53_record.apex]
 }
 resource "aws_route53_record" "kibana" {
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
   name    = "kibana.${var.domain}"
   type    = "A"
   alias {
-    name                   = "${var.domain}"
-    zone_id                = "${var.zone_id}"
+    name                   = var.domain
+    zone_id                = var.zone_id
     evaluate_target_health = false
   }
-  depends_on = ["aws_route53_record.apex"]
+  depends_on = [aws_route53_record.apex]
 }
 resource "aws_route53_record" "dashboard" {
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
   name    = "dashboard.${var.domain}"
   type    = "A"
   alias {
-    name                   = "${var.domain}"
-    zone_id                = "${var.zone_id}"
+    name                   = var.domain
+    zone_id                = var.zone_id
     evaluate_target_health = false
   }
-  depends_on = ["aws_route53_record.apex"]
+  depends_on = [aws_route53_record.apex]
 }
