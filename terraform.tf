9bce081b6a0fdeede56f89e63a4fa901f8ddb456
Philipp Krenn <pk@xeraa.net>
2021-06-15 02:20:21 +0200

    CHANGE to Terraform provisioned Elastic Cloud

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ec = {
+      source  = "elastic/ec"
+    }
+  }
+}
+
+
 provider "aws" {
     # Credentials are defined in the environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
     region = var.region
@@ -65,25 +74,46 @@ resource "aws_route53_record" "www" {
   }
   depends_on = [aws_route53_record.apex]
 }
-resource "aws_route53_record" "kibana" {
-  zone_id = var.zone_id
-  name    = "kibana.${var.domain}"
-  type    = "A"
-  alias {
-    name                   = var.domain
-    zone_id                = var.zone_id
-    evaluate_target_health = false
-  }
-  depends_on = [aws_route53_record.apex]
+
+
+# Create the Elastic Cloud setup
+resource "ec_deployment" "ec_auditbeat" {
+  name                   = "auditbeat-in-action"
+  region                 = var.region
+  version                = "7.13.1"
+  deployment_template_id = "aws-io-optimized-v2"
+
+  elasticsearch {}
+
+  kibana {}
 }
-resource "aws_route53_record" "dashboard" {
-  zone_id = var.zone_id
-  name    = "dashboard.${var.domain}"
-  type    = "A"
-  alias {
-    name                   = var.domain
-    zone_id                = var.zone_id
-    evaluate_target_health = false
-  }
-  depends_on = [aws_route53_record.apex]
+output "elasticsearch_https_endpoint" {
+  value = ec_deployment.ec_auditbeat.elasticsearch[0].https_endpoint
+}
+output "elastic_username" {
+  value = ec_deployment.ec_auditbeat.elasticsearch_username
+}
+output "elastic_password" {
+  value = ec_deployment.ec_auditbeat.elasticsearch_password
+  sensitive = true
+}
+output "elasticsearch_cloud_id" {
+  value = ec_deployment.ec_auditbeat.elasticsearch[0].cloud_id
+}
+output "kibana_https_endpoint" {
+  value = ec_deployment.ec_auditbeat.kibana[0].https_endpoint
+}
+locals {
+  elastic-cloud = <<-EOT
+elasticsearch_password: ${ec_deployment.ec_auditbeat.elasticsearch_password}
+elasticsearch_host: ${ec_deployment.ec_auditbeat.elasticsearch[0].https_endpoint}
+elasticsearch_user: ${ec_deployment.ec_auditbeat.elasticsearch_username}
+kibana_host: ${ec_deployment.ec_auditbeat.kibana[0].https_endpoint}
+elastic_version: ${ec_deployment.ec_auditbeat.version}
+  EOT
+}
+
+resource "local_file" "elastic_cloud_config" {
+  filename = "elastic-cloud.yml"
+  content  = local.elastic-cloud
 }
